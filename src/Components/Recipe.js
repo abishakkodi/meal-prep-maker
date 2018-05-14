495b67c8a8b3b4f6b23f1cc2de2bfc7db02c7378
Abishak Kodi <abishak.kodi@gmail.com>
2018-05-14 12:53:57 -0700

    Styled CSS cards to look more presentatable - didn't lose responsive styling - need to find out how to break it

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { Card } from 'antd';
+const { Meta } = Card;
 
-const style = { width: 300, 'borderStyle': 'solid', height: '200', 'borderColor': 'black', 'margin': '2px', 'textAlign': 'center', 'float':'left' };
+
+const style = { width: 300, height: '200', 'margin': '2px', 'textAlign': 'center' };
 
 class Recipes extends Component {
   render() {
     let info = this.props.recipeData;
     return (
-      <div className="Recipe">
-       <Card title={info.name} style={style} className="recipesList" >
+      <div>
+       <Card title={info.name} style={style} hoverable
+       cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} className='cardImage'>
          <p>Category: {info.category} </p>
          <p>Calories: {info.calories} </p>
+         <Meta title={info.name}/>
+
        </Card>
       </div>
     );
