01528e13c9413cc88e7e0abcb86a1b0f5a7df9fe
Abishak Kodi <abishak.kodi@gmail.com>
2018-06-07 18:42:01 -0700

    Pivot from mysql to mongo - mongoose for orm - mlab for virtual mongo server - keeping SQL code for future efficiency - connected to mongoose dev server - keeping helpers but optim to use them for mongo - created config file for mLab key

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { connectionInfo } = require('../config.js');
+const mongoose = require('mongoose');
+
+mongoose.connect(connectionInfo);
+mongoose.connection.once('open',()=>{
+  console.log('Connected to mlab');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -18,53 +26,53 @@ const initDatabase = require('./initDatabase');
 const mealPlanner = require('./mealPlanner');
 const reset = { force: false }; //
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
 
-sequelize.sync(reset)
-  .then(() => {
-    console.log('Synced to DB');
-  })
-  .then(()=>{
+// sequelize.sync(reset)
+//   .then(() => {
+//     console.log('Synced to DB');
+//   })
+//   .then(()=>{
 
-    initDatabase();
+//     initDatabase();
 
-  })
-  .then(() => {
-    createStaple({
-      name: 'testStaple',
-      vegetarian: false,
-      calories: 30,
-      protein: 30,
-      carb: 30,
-      fat: 30
-    });
-  })
-  .then(()=>{
-    for( let i = 0; i < 5; i++) {
-      createRecipe({
-      name: 'recipe ' + JSON.stringify(i),
-      calories: (i * 10),
-      description: 'some text',
-      steps: 'some text',
-      vegetarian: true,
-      pictureLink: 'some url',
-      recipeIngredients: [1*i,2*i,3*i],
-      category: 'breakfast'
-    });
-    }
+//   })
+//   .then(() => {
+//     createStaple({
+//       name: 'testStaple',
+//       vegetarian: false,
+//       calories: 30,
+//       protein: 30,
+//       carb: 30,
+//       fat: 30
+//     });
+//   })
+//   .then(()=>{
+//     for( let i = 0; i < 5; i++) {
+//       createRecipe({
+//       name: 'recipe ' + JSON.stringify(i),
+//       calories: (i * 10),
+//       description: 'some text',
+//       steps: 'some text',
+//       vegetarian: true,
+//       pictureLink: 'some url',
+//       recipeIngredients: [1*i,2*i,3*i],
+//       category: 'breakfast'
+//     });
+//     }
 
 
-  })
-  .catch(err => {
-    console.error('Unable to sync to the database:', err);
-  });
+//   })
+//   .catch(err => {
+//     console.error('Unable to sync to the database:', err);
+//   });
 
 
 //Node Server
@@ -72,45 +80,46 @@ const NodePort = 8000;
 
 
 //ROUTES
-app.get('/', (req, res) => {
-  onLoad();
-  res.send('Test Res ');
+// app.get('/', (req, res) => {
+//   onLoad();
+//   res.send('Test Res ');
+// });
+
+// app.get('/readRecipes', (req, res) => {
+//   readRecipes();
+//   res.send(`
+//         <h4>
+//           Recipe Read and Sent
+//         </h4>
+//     `);
+// });
+
+// app.get('/mealPlanner', mealPlanner);
+
+// app.get('/testResponse', (req, res)=> {
+//   res.send({message: 'requestReceived'});
+// })
+
+// app.post('/createIngredient', (req, res) => {
+//   const created = createIngredient(req.body);
+//   if (created) console.log('Created ' + req.body.name); //?
+// });
+
+// app.post('/createRecipe', (req, res) => {
+//   const created = createRecipe(req.body);
+//   if (created) console.log('Created ' + req.body.name); //?
+// });
+
+// app.post('/createStaple', (req, res) => {
+//   const created = createStaple(req.body);
+//   if (created) console.log('Created ' + req.body.name); //?
+// });
+
+
+
+
+
+
+app.listen(NodePort, ()=>{
+  console.log('listening on NodePort ' + NodePort)
 });
-
-app.get('/readRecipes', (req, res) => {
-  readRecipes();
-  res.send(`
-        <h4>
-          Recipe Read and Sent
-        </h4>
-    `);
-});
-
-app.get('/mealPlanner', mealPlanner);
-
-app.get('/testResponse', (req, res)=> {
-  res.send({message: 'requestReceived'});
-})
-
-app.post('/createIngredient', (req, res) => {
-  const created = createIngredient(req.body);
-  if (created) console.log('Created ' + req.body.name); //?
-});
-
-app.post('/createRecipe', (req, res) => {
-  const created = createRecipe(req.body);
-  if (created) console.log('Created ' + req.body.name); //?
-});
-
-app.post('/createStaple', (req, res) => {
-  const created = createStaple(req.body);
-  if (created) console.log('Created ' + req.body.name); //?
-});
-
-
-
-
-
-
-app.listen(NodePort);
-console.log('listening on NodePort ' + NodePort);
