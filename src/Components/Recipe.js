739bad33ec405a71fc5a65fee0c24ec1f175610b
Abishak Kodi <abishak.kodi@gmail.com>
2018-06-17 09:50:57 -0700

    Added popup behavior for recipes

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Popup from "reactjs-popup";
 
 import { Card } from 'antd';
@@ -7,21 +7,41 @@ const { Meta } = Card;
 
 const style = { width: 300, height: '200', 'margin': '2px', 'textAlign': 'left' };
 
-class Recipes extends Component {
-  render() {
-    let info = this.props.recipeData;
+const Recipes = (props) => {
+ let info = props.recipeData;
     return (
       <div className='cardImage'>
-       <Card style={style} hoverable
-       cover={<span> <img alt={info.name} src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" /> </span>} >
-         <Meta title={info.name}/>
-         <p>Category: {info.category} </p>
-         <p>Calories: {info.totalCalories} </p>
-
-       </Card>
+       <Popup
+        trigger={
+          <div className="button">
+            <Card style={style} hoverable
+             cover={<span> <img alt={info.name} src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" /> </span>} >
+               <Meta title={info.name}/>
+               <p>Category: {info.category} </p>
+               <p>Calories: {info.totalCalories} </p>
+            </Card>
+          </div>}
+        modal
+        closeOnDocumentClick
+      >
+    <div className="recipePopup">
+      <p>Category: {info.category} </p>
+      <p>Calories: {info.totalCalories} </p>
+    </div>
+  </Popup>
       </div>
-    );
-  }
+)
 }
 
+
 export default Recipes;
+
+
+    // <Card style={style} hoverable
+    //    cover={<span> <img alt={info.name} src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" /> </span>} >
+    //      <Meta title={info.name}/>
+    //      <p>Category: {info.category} </p>
+    //      <p>Calories: {info.totalCalories} </p>
+
+    //    </Card>
+
