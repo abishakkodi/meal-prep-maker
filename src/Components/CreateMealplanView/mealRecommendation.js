7c40e2b3d065944ca7f1d61691c31b49d704b165
Abishak Kodi <abishak.kodi@gmail.com>
2018-07-22 19:06:30 -0700

    Passed Initial Tests to filter difficulties objects

--- src/Components/CreateMealplanView/mealRecommendation.js
+++ src/Components/CreateMealplanView/mealRecommendation.js
@@ -1,16 +1,19 @@
+const _ = require('lodash');
+
 const mealRecommendation = (preferencesObj, recipesObj) => {
 
-  let recommendations;
+    let recommendations = recipesObj;
 
-  if(preferencesObj.vegetarian){
-    recommendations = filtered(recipesObj, vegetarianFilter);
+    if (preferencesObj.vegetarian) {
+        recommendations = filtered(recommendations, vegetarianFilter);
     }
 
-  if(!recommendations){
-    return recipesObj;
-  }
+    if(preferencesObj.difficulty) {
+      const difficultyFilter = selectDifficultyFilter(preferencesObj.difficulty);
+      recommendations = filtered(recommendations, difficultyFilter)
+    }
 
-  return recommendations;
+    return recommendations;
 }
 
 // if vegetarian filter out all meat recipes and return array of base objects
@@ -18,28 +21,53 @@ const mealRecommendation = (preferencesObj, recipesObj) => {
 //
 
 
-const filtered = ( recipesObj, filterFunc ) => {
-  let filteredRecipes = { protein: [], vegetables: [], carbs: [] };
-  filteredRecipes.protein = recipesObj.protein.filter((recipe =>{
-    return filterFunc(recipe);
-  }));
-  filteredRecipes.vegetables = recipesObj.vegetables.filter((recipe =>{
-    return filterFunc(recipe);
-  }));
-  filteredRecipes.carbs = recipesObj.carbs.filter((recipe =>{
-    return filterFunc(recipe);
-  }));
+const filtered = (recipesObj, filterFunc) => {
+    let filteredRecipes = { protein: [], vegetables: [], carbs: [] };
+    filteredRecipes.protein = recipesObj.protein.filter((recipe => {
+        return filterFunc(recipe);
+    }));
+    filteredRecipes.vegetables = recipesObj.vegetables.filter((recipe => {
+        return filterFunc(recipe);
+    }));
+    filteredRecipes.carbs = recipesObj.carbs.filter((recipe => {
+        return filterFunc(recipe);
+    }));
 
-  return filteredRecipes;
+    return filteredRecipes;
 }
 
 
 const vegetarianFilter = (recipeObject) => {
-  if(recipeObject.recipe.vegetarian) {
-    return true;
-  } else {
-    return false
-  }
+    if (recipeObject.recipe.vegetarian) {
+        return true;
+    } else {
+        return false
+    }
 }
 
-module.exports = mealRecommendation;
+const selectDifficultyFilter = (preferenceDifficultyArray) => {
+    if (preferenceDifficultyArray.includes(3)) {
+        return (recipeObject => {
+            return true;
+        })
+    } else if (preferenceDifficultyArray.includes(2)) {
+        return (recipesObject => {
+            if (recipesObject.recipe.difficulty <= 2) {
+                return true
+            } else {
+                return false
+            }
+        })
+    } else if (preferenceDifficultyArray[0] === 1) {
+        return (recipeObj => {
+          if(recipeObj.recipe.difficulty === 1 ) {
+            return true
+          } else {
+            return false
+          }
+        })
+    }
+}
+
+
+module.exports = {mealRecommendation,selectDifficultyFilter };
