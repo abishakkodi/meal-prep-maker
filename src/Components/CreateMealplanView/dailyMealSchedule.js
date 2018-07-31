19ceaebc5f5b74b2e337deb93b7705b23ff081fc
Abishak Kodi <abishak.kodi@gmail.com>
2018-07-30 22:07:54 -0700

    Working (?) Daily Meal Schedule Function

--- src/Components/CreateMealplanView/dailyMealSchedule.js
+++ src/Components/CreateMealplanView/dailyMealSchedule.js
@@ -1,84 +1,77 @@
 const _ = require('lodash');
-
 const defaultMacrosPercentages = { p: 50, v: 30, c: 20 };
+const defaultMealsPerDay = 3;
+const defaultNumberOfDays = 4;
 
-const dailyMealSchedule = (recommendedMeals, calorieRequest, mealsPerDay = 3, numberOfDays = 4, macros = testMacrosPercentages) => {
-    let mealTotals = {};
-    let dailyMealsArray = [];
-    const maxMealCalories = calorieRequest / mealsPerDay;
-    const calsByMacro = caloriesByMacro(maxMealCalories, macros);
+const dailyMealSchedule = (recommendedMeals, calorieRequest, mealsPerDay = defaultMealsPerDay, numberOfDays = defaultNumberOfDays, macros = testMacrosPercentages) => {
+  let ingredientAmountTotals = {};
+  let dailyMealsArray = [];
+  const maxMealCalories = calorieRequest / mealsPerDay;
+  const calsByMacro = caloriesByMacro(maxMealCalories, macros);
 
-    for (let i = 0; i < numberOfDays; i++) {
-      dailyMealsArray.push(dailyMealsObject(recommendedMeals, mealsPerDay, calsByMacro))
-    }
+  for (let i = 0; i < numberOfDays; i++) {
+    let dailyMeal = dailyMealsObject(recommendedMeals, mealsPerDay, calsByMacro, ingredientAmountTotals);
+    dailyMeal.id = i;
+    ingredientAmountTotals = dailyMeal.mealTotals;
+    dailyMealsArray.push(dailyMeal);
+  }
 
-    return dailyMealsArray;
+  return ({dailyMealsArray, ingredientAmountTotals});
 }
 
-const dailyMealsObject = (recommendedMeals, mealsPerDay, caloriesByMacro) => {
-    let dailyMeal = {};
-    for (let j = 0; j < mealsPerDay; j++) {
-        dailyMeal[j] = buildMeal(recommendedMeals, caloriesByMacro);
-    }
-    return dailyMeal;
+const dailyMealsObject = (recommendedMeals, mealsPerDay, caloriesByMacro, mealTotals) => {
+  let dailyMeals = { mealTotals };
+  for (let j = 0; j < mealsPerDay; j++) {
+    dailyMeals[j] = mealObject(recommendedMeals, caloriesByMacro, mealTotals);
+    dailyMeals['mealTotals'] = dailyMeals[j].mealTotals;
+  }
+
+  return dailyMeals;
 }
 
-const buildMeal = (recommendedMeals, caloriesByMacro) => {
 
-    let protein = _.sample(recommendedMeals.protein);
-    protein.multiplier = parseFloat(caloriesByMacro['p'] * 1.0 / protein.recipe.baseCalories).toFixed(2);
+//can refactor this later
+const mealObject = (recommendedMeals, caloriesByMacro, mealTotals) => {
 
-    let carb = _.sample(recommendedMeals.carbs);
-    carb.multiplier = parseFloat(caloriesByMacro['c'] * 1.0 / carb.recipe.baseCalories).toFixed(2);
-    let vegetable = _.sample(recommendedMeals.vegetables);
-    vegetable.multiplier = parseFloat(caloriesByMacro['v'] * 1.0 / vegetable.recipe.baseCalories).toFixed(2);
+  let protein = _.sample(recommendedMeals.protein);
+  protein.multiplier = parseFloat(caloriesByMacro['p'] * 1.0 / protein.recipe.baseCalories).toFixed(2);
+  protein.ingredients.map((ingredient) => {
+    ingredient.adjustedAmount = ingredient.amount * protein.multiplier;
+    mealTotals = ingredientTotals(mealTotals, ingredient.name, ingredient.adjustedAmount);
+  });
 
-    return ({ protein, carb, vegetable })
+  let carb = _.sample(recommendedMeals.carbs);
+  carb.multiplier = parseFloat(caloriesByMacro['c'] * 1.0 / carb.recipe.baseCalories).toFixed(2);
+  carb.ingredients.map((ingredient) => {
+    ingredient.adjustedAmount = ingredient.amount * carb.multiplier;
+    mealTotals = ingredientTotals(mealTotals, ingredient.name, ingredient.adjustedAmount);
+  });
+
+  let vegetable = _.sample(recommendedMeals.vegetables);
+  vegetable.multiplier = parseFloat(caloriesByMacro['v'] * 1.0 / vegetable.recipe.baseCalories).toFixed(2);
+  vegetable.ingredients.map((ingredient) => {
+    ingredient.adjustedAmount = ingredient.amount * vegetable.multiplier;
+    mealTotals = ingredientTotals(mealTotals, ingredient.name, ingredient.adjustedAmount);
+  });
+
+  return ({ protein, carb, vegetable, mealTotals })
 }
 
 const caloriesByMacro = (maxMealCalories, macros) => ({
-    p: macros['p'] * maxMealCalories * 0.01,
-    c: macros['c'] * maxMealCalories * 0.01,
-    v: macros['v'] * maxMealCalories * 0.01
+  p: macros['p'] * maxMealCalories * 0.01,
+  c: macros['c'] * maxMealCalories * 0.01,
+  v: macros['v'] * maxMealCalories * 0.01
 })
 
 const ingredientTotals = (totalsObj, ingredient, amount) => {
-  if(ingredient in totalsObj) {
-    totalsObj[ingredient] +=  amount;
+  if (ingredient in totalsObj) {
+    totalsObj[ingredient] += amount;
   } else {
     totalsObj[ingredient] = amount;
   }
-
-
   return totalsObj;
 }
 
-module.exports = { dailyMealSchedule, dailyMealsObject, buildMeal, caloriesByMacro,ingredientTotals };
+const recipeIngredientTotals = (totalsObj, ingredientsArray) => {};
 
-
-/*
-macro ratios?
-protein 50%
-vegetables 30%
-carbs 20%
-
-Have an object array to push ingredients in
-{
-  meal1: { vegetableRecipe:
-           vegetableRecipe:
-           proteinRecipe:
-           multiplier:
-  }
-
-
-400 X     250
-
-
-}
-
-1. Figure out how to add all calories together
-2. Figure out how to get ingredient amounts linked to calories
-
-
-
-*/
+module.exports = { dailyMealSchedule, dailyMealsObject, mealObject, caloriesByMacro, ingredientTotals };
